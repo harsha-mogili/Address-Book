ce8d769838fe8ce5656dc6a3ccce802d07d9ba52
Sandeep Mandela <sandeepmandela@Sandeeps-MacBook-Pro.local>
2021-06-03 22:20:38 -0600

    Added helloworld restAPI

--- src/main/java/com/contact/AddressBook/Controller/AddressBookController.java
+++ src/main/java/com/contact/AddressBook/Controller/AddressBookController.java
@@ -1,5 +1,13 @@
 package com.contact.AddressBook.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AddressBookController {
 
+    @GetMapping("/helloWorld")
+    public String helloWorld(){
+        return "Hello world";
+    }
 }
